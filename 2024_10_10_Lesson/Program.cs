a45b729ed6558d8f6e7e94b56b67253988f359ea
tenseoverflow <contact@hen.ee>
2024-10-10 09:53:09 +0300

    Refactor date display in Program.cs
    Signed-off-by: tenseoverflow <contact@hen.ee>

--- 2024_10_10_Lesson/Program.cs
+++ 2024_10_10_Lesson/Program.cs
@@ -8,8 +8,8 @@ namespace _2024_10_10_Lesson
         {
             long year = 0;
             double someValue = 0.1;
-            DateTime date = DateTime.Now;
-            Console.WriteLine(date.ToString());
+            DateTime date = DateTime.Now.AddDays(-25);
+            Console.WriteLine(date.Date.ToString("dd.MM.yyyy"));
             Console.ReadKey();
         }
     }
