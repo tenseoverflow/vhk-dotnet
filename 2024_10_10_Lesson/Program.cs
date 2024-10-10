e126cb3f95cdc0ecf2ba1c7d899519809b8c677a
tenseoverflow <contact@hen.ee>
2024-10-10 09:43:30 +0300

    Add 2024_10_10_Lesson project to the solution

---
+++ 2024_10_10_Lesson/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _2024_10_10_Lesson
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            long year = 0;
+            double someValue = 0.1;
+            DateTime date = DateTime.Now;
+            Console.WriteLine(date.ToString());
+            Console.ReadKey();
+        }
+    }
+}
