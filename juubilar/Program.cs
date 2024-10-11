0d526d15a5f779293cda8d37c0a187400acbf4c4
tenseoverflow <contact@hen.ee>
2024-10-11 21:29:10 +0300

    Add juubilar and pikkus projects to the solution

---
+++ juubilar/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace juubilar
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            Console.WriteLine("Sisesta oma vanus:");
+            var input = Console.ReadLine();
+            int age = int.TryParse(input, out age) ? age : -1;
+
+            if (age < 50 && age > 0)
+            {
+                Console.WriteLine("Kõik hästi!");
+            }
+            else if (age == 50)
+            {
+                for (int i = 0; i < 50; i++)
+                {
+                    Console.WriteLine("Hurraa juubilar!");
+                }
+            }
+            else if (age > 50)
+            {
+                Console.WriteLine("Väärikas kodanik!");
+            }
+            else
+            {
+                Console.WriteLine("Palun sisesta korrektne vanus täisarvudes.");
+            }
+        }
+    }
+}
