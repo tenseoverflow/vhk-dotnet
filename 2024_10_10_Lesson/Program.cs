5f71227953a45d7385f9a8e81d90d31a905b08b1
tenseoverflow <contact@hen.ee>
2024-10-31 10:19:05 +0200

    Program

--- 2024_10_10_Lesson/Program.cs
+++ 2024_10_10_Lesson/Program.cs
@@ -8,8 +8,35 @@ namespace _2024_10_10_Lesson
         {
             long year = 0;
             double someValue = 0.1;
+            DateTime nowDate = new DateTime(2015, 10, 15);
             DateTime date = DateTime.Now.AddDays(-25);
             Console.WriteLine(date.Date.ToString("dd.MM.yyyy"));
+            Console.WriteLine(nowDate);
+            int monthDay = date.Day;
+
+            /*for (int i = 1; i <= monthDay; i++)
+            {
+                Console.WriteLine("test: " + i + "bruh");
+            }*/
+
+            List<string> namesList = new List<string>() { "John", "Doe", "Jane" };
+            List<string> lastNamesList = new List<string>();
+            namesList.Add("Mihkel");
+
+            System.String[] namesArray = namesList.ToArray();
+
+            foreach (string name in namesList)
+            {
+                string lastName = "Tamm";
+                string nameAndLastName = name + " " + lastName;
+                lastNamesList.Add(nameAndLastName);
+            }
+
+            foreach (var name in lastNamesList)
+            {
+                Console.WriteLine(name);
+            }
+
             Console.ReadKey();
         }
     }
